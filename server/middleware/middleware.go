0ba18d7c274967bc80a396cd67a4c7ec496bc4c6
Benny Kritzer <benny.kritzer@gmail.com>
2021-04-03 11:28:52 -0400

    Various fixes

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -102,7 +101,6 @@ func CreateTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	var task models.ToDoList
 	_ = json.NewDecoder(r.Body).Decode(&task)
-	// fmt.Println(task, r.Body)
 	insertOneTask(task)
 	json.NewEncoder(w).Encode(task)
 }
@@ -152,24 +150,8 @@ func DeleteAllTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	count := deleteAllTask()
 	json.NewEncoder(w).Encode(count)
-	// json.NewEncoder(w).Encode("Task not found")
-
 }
 
-// handle shutdown
-/* func gracefulShutdown() <-chan struct{} {
-	end := make(chan struct{})
-	s := make(chan os.Signal, 1)
-	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-s
-		fmt.Println("Sutting down gracefully.")
-		// clean up here
-		close(end)
-	}()
-	return end
-}
-*/
 // get all task from the DB and return it
 func getAllTask() []primitive.M {
 	cur, err := collection.Find(context.Background(), bson.D{{}})
@@ -184,7 +166,6 @@ func getAllTask() []primitive.M {
 		if e != nil {
 			log.Fatal().Err(e).Msg("")
 		}
-		// fmt.Println("cur..>", cur, "result", reflect.TypeOf(result), reflect.TypeOf(result["_id"]))
 		results = append(results, result)
 
 	}
@@ -194,6 +175,7 @@ func getAllTask() []primitive.M {
 	}
 
 	cur.Close(context.Background())
+	log.Debug().Msg("Get all tasks")
 	return results
 }
 
@@ -205,12 +187,11 @@ func insertOneTask(task models.ToDoList) {
 		log.Fatal().Err(err).Msg("")
 	}
 
-	fmt.Println("Inserted a Single Record ", insertResult.InsertedID)
+	log.Debug().Msgf("Inserted a Single Record %s", insertResult.InsertedID)
 }
 
 // task complete method, update task's status to true
 func taskComplete(task string) {
-	fmt.Println(task)
 	id, _ := primitive.ObjectIDFromHex(task)
 	filter := bson.M{"_id": id}
 	update := bson.M{"$set": bson.M{"status": true}}
@@ -218,13 +199,11 @@ func taskComplete(task string) {
 	if err != nil {
 		log.Fatal().Err(err).Msg("")
 	}
-
-	fmt.Println("modified count: ", result.ModifiedCount)
+	log.Debug().Msgf("Task %s complete, count: %d", task, result.ModifiedCount)
 }
 
 // task undo method, update task's status to false
 func undoTask(task string) {
-	fmt.Println(task)
 	id, _ := primitive.ObjectIDFromHex(task)
 	filter := bson.M{"_id": id}
 	update := bson.M{"$set": bson.M{"status": false}}
@@ -233,12 +212,11 @@ func undoTask(task string) {
 		log.Fatal().Err(err).Msg("")
 	}
 
-	fmt.Println("modified count: ", result.ModifiedCount)
+	log.Debug().Msgf("Task %s undo, count: %d", task, result.ModifiedCount)
 }
 
 // delete one task from the DB, delete by ID
 func deleteOneTask(task string) {
-	fmt.Println(task)
 	id, _ := primitive.ObjectIDFromHex(task)
 	filter := bson.M{"_id": id}
 	d, err := collection.DeleteOne(context.Background(), filter)
@@ -246,7 +224,7 @@ func deleteOneTask(task string) {
 		log.Fatal().Err(err).Msg("")
 	}
 
-	fmt.Println("Deleted Document", d.DeletedCount)
+	log.Debug().Msgf("Task %s delete, count: %d", task, d.DeletedCount)
 }
 
 // delete all the tasks from the DB
@@ -256,6 +234,6 @@ func deleteAllTask() int64 {
 		log.Fatal().Err(err).Msg("")
 	}
 
-	fmt.Println("Deleted Document", d.DeletedCount)
+	log.Debug().Msgf("Deleted Document, count: %d", d.DeletedCount)
 	return d.DeletedCount
 }
